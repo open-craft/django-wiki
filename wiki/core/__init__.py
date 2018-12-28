e4308a4a8ccd18c6a435e0e1906dfdfa6dc3957c
DawoudSheraz <dawoud.sheraz@arbisoft.com>
2018-12-28 18:40:49 +0500

    Clean XSS in wiki content

--- wiki/core/__init__.py
+++ wiki/core/__init__.py
@@ -1,12 +1,15 @@
 import markdown
+import bleach
 
 class ArticleMarkdown(markdown.Markdown):
-    
+
     def __init__(self, article, *args, **kwargs):
         kwargs['safe_mode'] = "remove"
         markdown.Markdown.__init__(self, *args, **kwargs)
         self.article = article
 
+
 def article_markdown(text, article, *args, **kwargs):
     md = ArticleMarkdown(article, *args, **kwargs)
+    text = bleach.clean(text, strip=True)
     return md.convert(text)
